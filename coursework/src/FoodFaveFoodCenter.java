a0049dd2e6f7e62c32d58ffab2a53a560af87f3a
HGP  Madusanka <108182471+123Gayantha@users.noreply.github.com>
2023-08-07 09:33:26 +0530

    Add files via upload

---
+++ coursework/src/FoodFaveFoodCenter.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+public class FoodFaveFoodCenter {
+    public static void main(String[] args) {
+
+        //creating a list of customers//
+        Scanner CashierQueue=new Scanner(System.in);
+
+        System.out.println("Welcome to the FoodFaveCenter");
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
